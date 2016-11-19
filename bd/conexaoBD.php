1506633be8ce6db3e813f2c2b0516c87f373fb64
Eder Antonio da Silva <ederanton@gmail.com>
2016-11-19 18:11:31 -0200

    trazendo os dados do banco

---
+++ bd/conexaoBD.php
@@ -0,0 +1,11 @@
+<?php
+
+    $_conecta = mysqli_connect('localhost','root','root','CadastroDePostos');
+        if (!$_conecta) {
+            echo 'Não foi possível a conexão com o banco: ' . mysqli_error($_conecta);
+        } else {
+            echo 'Conexão ok<br/>';
+        }
+
+
+?>
---
+++ bd/listagemPostos.php
@@ -0,0 +1,24 @@
+<?php
+include ("conexaoBD.php"); //faz a conexão ao banco de dados
+
+$_sql = "select * from PostoDescricao";
+    $_res = $_conecta->query($_sql);
+    if($_res === FALSE){
+        echo "Erro na consulta do banco.".$_conecta->error."<br/>";
+    } else{
+
+        echo "Banco lido com sucesso <br/>";
+
+        $_nr = $_res->num_rows;
+        echo "A consulta retornou ".(int) $_nr . " registros <br/>";
+    }
+
+    $postos = array();
+
+    while($_localidades = $_res->fetch_assoc()){
+
+
+
+        array_push($postos, $_localidades);
+    }
+?>
